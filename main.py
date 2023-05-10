23f926040116ca71bf774cc9c207ff0bc449d53c
hy-Jo <22205409-hy-Jo@users.noreply.replit.com>
2023-05-10 14:51:24 +0000

    add question 2

--- main.py
+++ main.py
@@ -4,23 +4,37 @@
 # Accepts a dictionary containing the file owner name for each file name. Returns a dictionary containing a list of file names for each owner name, in any order. For example, for dictionary {'Input.txt': 'Randy', 'Code.py': 'Stan', 'Output.txt': 'Randy'} the group_by_owners function should return {'Randy': ['Input.txt', 'Output.txt'], 'Stan': ['Code.py']}.
 
 
-def group_by_owners(files):
+# def group_by_owners(files):
 
-  ownerdict = {}
+#   ownerdict = {}
   
-  for files, owner in files.items():
-    if owner in ownerdict: #key is owner
-      ownerdict[owner].append(files)
-    else :
-      ownerdict[owner]= [files]
+#   for files, owner in files.items():
+#     if owner in ownerdict: #key is owner
+#       ownerdict[owner].append(files)
+#     else :
+#       ownerdict[owner]= [files]
       
-  ownerdict = dict(sorted(ownerdict.items()))    
-  return ownerdict
+#   ownerdict = dict(sorted(ownerdict.items()))    
+#   return ownerdict
 
-if __name__ == "__main__":    
-    files = {
-        'Input.txt': 'Randy',
-        'Code.py': 'Atan',
-        'Output.txt': 'Randy'
-    }   
-    print(group_by_owners(files))
+# if __name__ == "__main__":    
+#     files = {
+#         'Input.txt': 'Randy',
+#         'Code.py': 'Atan',
+#         'Output.txt': 'Randy'
+#     }   
+#     print(group_by_owners(files))
+
+'''Question 2'''
+
+import numpy as np
+
+def find_roots(a, b, c):
+    input = [a, b, c]
+    answer = tuple(np.roots(input))
+    if answer[0] == answer[1]:
+      answer = (answer[0],)
+     
+    return answer
+
+print(find_roots(-1, 2, -1));
